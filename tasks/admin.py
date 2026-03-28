8afbb70c538fd5d84dfdbf46c0fc00325410be56
DIVAKAR1516 <71762331016@cit.edu.in>
2026-03-28 23:12:42 +0530

    Added auth endpoints

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from taskmanager.tasks.models import TaskUserProfile, Task
+
+admin.site.register(Task)
+admin.site.register(TaskUserProfile)
